93546baff432f710f9ce16ae4d37c1e9b2641ba1
twocucao <twocucao@gmail.com>
2018-06-08 21:20:59 +0800

    polish code

--- yapylib/profile/ptime.py
+++ yapylib/profile/ptime.py
@@ -1,3 +1,7 @@
+import cProfile as Profile
+import contextlib
+import functools
+import pstats
 import time
 from functools import wraps
 
@@ -58,3 +62,48 @@ class Timer:
 
     def __exit__(self, *args):
         self.stop()
+
+
+def prof_deco(sort_by="cumtime", limit=10, timer=time.perf_counter):
+    """
+    参考了雨痕的 <Python学习笔记>
+    :param sort_by:
+    :param limit:
+    :param timer:
+    :return:
+    """
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrap(*args, **kwargs):
+            p = Profile()
+            p.enable()
+            try:
+                return func(*args, **kwargs)
+            finally:
+                p.disable()
+                s = pstats.Stats(p).sort_stats(sort_by)
+                s.print_stats(limit)
+
+        return wrap
+
+    return decorator
+
+
+@contextlib.contextmanager
+def prof_context(sort_by="cumtime", limit=10, timer=time.perf_counter):
+    """
+    参考了雨痕的 <Python学习笔记>
+    :param sort_by:
+    :param limit:
+    :param timer:
+    :return:
+    """
+    p = Profile()
+    p.enable()
+    try:
+        yield
+    finally:
+        p.disable()
+        s = pstats.Stats(p).sort_stats(sort_by)
+        s.print_stats(limit)
